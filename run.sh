ac093d2fecab8efd4949321f76bc7e03c091150f
dev <dev@dev.io>
2018-09-08 12:46:48 +0900

    update.

--- run.sh
+++ run.sh
@@ -11,12 +11,22 @@ TARGET_CSV_PATH='target.csv'
 REPORT_ROOT_PATH="report"
 PSI_API_URL=https://www.googleapis.com/pagespeedonline/v4/runPagespeed
 
-source ./functions.sh
+auditPSI () {
+    target_url=$1
+    start_time=$2
+    REPORT_RELATIVE_PATH=`echo ${target_url} | sed -e "s#https://##g" -e "s#http://##g"`
+    REPORT_OUT_DIR_PATH="${REPORT_ROOT_PATH}/${REPORT_RELATIVE_PATH}"
+    REPORT_OUT_FILE_PATH="${REPORT_OUT_DIR_PATH}/${start_time}_psi.report.json"
+    # Create report directory.
+    mkdir -p ${REPORT_OUT_DIR_PATH}
+    # Run PageSpeedInsights.
+    curl -s "${PSI_API_URL}?strategy=mobile&key=${PSI_API_KEY}&url=${target_url}" -o ${REPORT_OUT_FILE_PATH}
+}
 
-target_urls=$(getTargetUrls "${TARGET_CSV_PATH}")
-echo 'Targets: ['"${target_urls}"']'
-target_url_array=(`echo ${target_urls} | tr -s ',' ' '`)
-for target_url in ${target_url_array[@]}; do
-    echo ${target_url}
-    analyzePSI "${target_url}" `getDateTimeText`
+for line in `cat ${TARGET_CSV_PATH} | grep -v ^#`
+do
+    target_url=`echo ${line} | cut -d ',' -f 1`
+    target_description=`echo ${line} | cut -d ',' -f 2`
+    echo ${target_description}' ('${target_url}')'
+    auditPSI "${target_url}" `date +%Y-%m-%d-%H-%M-%S`
 done
